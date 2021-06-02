80270ec0794d99d9d48883d15f30ca3cf2d37c2d
magnoazneto <magnoazneto@gmail.com>
2021-06-02 16:31:32 -0300

    [feat] adiciona dados do bacen para consulta de nomes das instituicoes

---
+++ src/main/kotlin/br/com/zup/ot4/keymanager/search/Organizations.kt
@@ -0,0 +1,28 @@
+package br.com.zup.ot4.keymanager.search
+
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.nio.file.Files.*
+import java.nio.file.Paths
+
+class Organizations() {
+    companion object {
+        private var names = mutableMapOf<String, String>()
+        init {
+            val bufferedReader = newBufferedReader(Paths.get("C:\\Users\\magno.neto\\Documents\\fase05\\key-manager-grpc\\ParticipantesSTRport.csv"))
+            val csvParser = CSVParser(bufferedReader, CSVFormat.DEFAULT)
+            csvParser.forEach { record ->
+                names[record.get(0)] = record.get(1).trim()
+            }
+        }
+        fun name(participant: String): String {
+           return names[participant]!!
+        }
+    }
+
+
+
+
+
+
+}
